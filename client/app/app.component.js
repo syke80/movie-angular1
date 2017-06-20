6502ad7c7427ee020ac3cbb932909b1acdaadced
Syke <syke80@gmail.com>
2017-06-20 09:03:41 +0200

    Use ng6starter

--- client/app/app.component.js
+++ client/app/app.component.js
@@ -1,5 +1,6 @@
 import template from './app.html';
 import controller from './app.controller.js';
+import '../../node_modules/materialize-css/dist/css/materialize.min.css';
 
 export default {
     template,
---
+++ client/app/services/movie.service.spec.js
@@ -0,0 +1,8 @@
+describe('MovieService', () => {
+    it('should pass', () => {
+      expect(2).to.equal(2); // Recommended
+    });
+    it('should fail', () => {
+      expect(2).to.equal(3); // Recommended
+    });
+});
